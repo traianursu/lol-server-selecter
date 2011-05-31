f12fd6f7877f2c622ef43af50e1f9d38688fc725
jeroenbuyssens.be@gmail.com <jeroenbuyssens.be@gmail.com@3c100462-0627-4ca6-ee11-62542a517d6e>
2011-05-31 11:20:27 +0000

    Created Project

---
+++ trunk/LoL-Starter/LoL-Starter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoL_Starter
+{
+    public partial class SartScreen : Form
+    {
+        public SartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void LoL_Starter_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
