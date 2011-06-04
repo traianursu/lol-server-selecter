86f88f3b13ddce9fb4872114387e7dd3df395af0
jeroenbuyssens.be@gmail.com <jeroenbuyssens.be@gmail.com@3c100462-0627-4ca6-ee11-62542a517d6e>
2011-06-04 08:02:41 +0000

    added code

--- trunk/LoLStarter/LoLStarter.cs
+++ trunk/LoLStarter/LoLStarter.cs
@@ -6,19 +6,24 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
 
 namespace LoLStarter
 {
     public partial class LoLStarter : Form
     {
+        Configuration configuration;
+
         public LoLStarter()
         {
             InitializeComponent();
+            configuration = new Configuration();
+            ttLang.SetToolTip(lbCurLang, "You can change the preferred language in the settings tab.");
         }
 
         private void LoLStarter_Load(object sender, EventArgs e)
         {
-            ttLang.SetToolTip(lbCurLang, "You can change the preferred language in the settings tab.");
             lbCurLang.Text = gbEULang.SelectedRadioButton().Text;
         }
 
@@ -26,13 +31,55 @@ namespace LoLStarter
         {
             lbCurLang.Text = gbEULang.SelectedRadioButton().Text;
         }
-    }
 
-    public static class Extensions
-    {
-        public static RadioButton SelectedRadioButton(this GroupBox g)
+        private void btnSearch_Click(object sender, EventArgs e)
         {
-            return g.Controls.OfType<RadioButton>().Where(rb => rb.Checked).FirstOrDefault();
+            fbdInstallFolder.ShowDialog();
+            String path = fbdInstallFolder.SelectedPath;
+            if (path != null && path != "")
+            {
+                tbInstallpath.Text = path;
+            }
+        }
+
+        private void btnEU_Click(object sender, EventArgs e)
+        
+        {
+            if (File.Exists(configuration.Folderpath + "\\lol.launcher.exe"))
+            {
+                setServer("eu");
+                Process.Start(configuration.Folderpath + "\\lol.launcher.exe");
+                this.Close();
+            }
+        }
+
+        private void btnUS_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(configuration.Folderpath + "\\lol.launcher.exe"))
+            {
+                setServer("na");
+                Process.Start(configuration.Folderpath + "\\lol.launcher.exe");
+                this.Close();
+            }
+        }
+
+        private void setServer(String server)
+        {
+            configuration.saveCFG();
+            configuration.writeGlobalProperties(server);
+            configuration.writeLauncherCFG(server);
+            if (server.ToLower().Equals("eu"))
+            {
+                if (configuration.EULang.Equals("unknown"))
+                    configuration.EULang = "en_GB";
+                if (String.Equals(configuration.EULang.ToLower(), "en_us"))
+                    configuration.EULang = "en_GB";
+                configuration.writeLocaleCFG(configuration.EULang);
+            }
+            else
+            {
+                configuration.writeLocaleCFG(configuration.USLang);
+            }
         }
     }
 }
